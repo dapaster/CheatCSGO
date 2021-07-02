2d25260594d2bcd876cfccfe5ef2df82311a870c
SmurfsCC <85826349+SmurfsCC@users.noreply.github.com>
2021-07-02 11:09:37 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# CheatCSGO
-A CS:GO cheat for linux
+<p align="center">
+  <img src="/res/logo.png" alt="x352-linux">
+  <br>
+  <a href="/LICENSE"><img src="https://img.shields.io/badge/License-MIT-green.svg" alt="MIT License"></a>
+  <a><img src="https://img.shields.io/badge/OS-Linux-green.svg" alt="Linux"></a>
+  <a><img src="https://img.shields.io/badge/Cheat-CS:GO-green.svg" alt="CS:GO cheat"></a>
+  <a><img src="https://img.shields.io/badge/State-Unfinished-red.svg" alt="Unfinished"></a>
+  <a><img src="https://img.shields.io/badge/Detection-Undetected-green.svg" alt="Detection: Unknown"></a>
+</p>
+
+An open source cheat for Counter-Strike: Global Offensive on Linux.
+
+## Current Features
+
+ - Basic GDB injector
+ - ImGui menu
+ - Chams
+ - ESP
+ - Weapon ESP
+ - Chicken/Fish ESP
+ - World colour modulation
+ - Force square radar
+ - Recoil crosshair
+ - Rank revealer
+ - Radio fake ban/fake chat
+ - Clantag (and pBeeMovie clantag)
+ - Backtracking
+ - Backtrack Chams
+ - Flappy Bird clone
+ - Legitbot
+ - Hitmarkers
+ - Nightmode
+ - Skybox changer
+ - Skinchanger
+ - Ragebot (currently in development and doesnt have awall so no mindmg/hitchance/autoshoot)
+ - AntiAim (currently sways, need to fix lby breaker)
+ - Movement cheats (Jumpbug, humanoid bhop, edgebug, etc.)
+
+## How to use
+
+### Debian / Ubuntu / Pop OS / Linux Mint required packages:
+
+`sudo apt install -y libsdl2-dev cmake git gcc-10 g++-10 gdb clang`
+
+### Arch / Manjaro required packages:
+
+`sudo pacman -S --needed base-devel git cmake gdb sdl2 git clang`
+
+### Fedora required packages:
+
+`sudo dnf install gcc-g++ gdb SDL2-devel cmake git clang`
+
+### OpenSUSE required packages:
+
+`sudo zypper install gcc gdb SDL2-devel cmake git llvm-clang llvm-gold`
+
+```sh
+git clone https://github.com/veasman/x352
+cd x352
+chmod +x toolbox.sh
+./toolbox.sh -p -u -b -l # pull, unload cheat if currently injected, build and then load, use -h for help
+```
+
+
+## Basic toolbox.sh usage
+
+| Argument           | Description                             |
+| ------------------ | --------------------------------------- |
+| -u (--unload)      | Unload the cheat from CS:GO if loaded   |
+| -l (--load)        | Load/inject the cheat via gdb           |
+| -ld (--load_debug) | Load/inject the cheat and debug via gdb |
+| -b (--build)       | Build to the build/ dir                 |
+| -bd (--build_debug)| Build to the build/ dir as debug        |
+| -p (--pull)        | Update the cheat                        |
+| -h (--help)        | Show help                               |
+
+All args are executed in the order they are written in, for example, "-u -b -l" would unload the cheat, build it, and then load it back into csgo.
+
+
+## Found a bug or want to contribute to the project?
+
+ - [Create a pull request](https://github.com/veasman/x352/compare) ([tutorial](https://github.com/yangsu/pull-request-tutorial))
+ - [Report a bug](https://github.com/veasman/x352/issues/new)
---
+++ sdk/interfaces/ienginetrace.hpp
@@ -0,0 +1,265 @@
+#pragma once
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#include "../sdk.hpp"
+#include "interfaces.hpp"
+#include "../classes/entity.hpp"
+
+#define   DISPSURF_FLAG_SURFACE           (1<<0)
+#define   DISPSURF_FLAG_WALKABLE          (1<<1)
+#define   DISPSURF_FLAG_BUILDABLE         (1<<2)
+#define   DISPSURF_FLAG_SURFPROP1         (1<<3)
+#define   DISPSURF_FLAG_SURFPROP2         (1<<4)
+
+#define   CONTENTS_EMPTY                0
+
+#define   CONTENTS_SOLID                0x1
+#define   CONTENTS_WINDOW               0x2
+#define   CONTENTS_AUX                  0x4
+#define   CONTENTS_GRATE                0x8
+#define   CONTENTS_SLIME                0x10
+#define   CONTENTS_WATER                0x20
+#define   CONTENTS_BLOCKLOS             0x40
+#define   CONTENTS_OPAQUE               0x80
+#define   LAST_VISIBLE_CONTENTS         CONTENTS_OPAQUE
+
+#define   ALL_VISIBLE_CONTENTS            (LAST_VISIBLE_CONTENTS | (LAST_VISIBLE_CONTENTS-1))
+
+#define   CONTENTS_TESTFOGVOLUME        0x100
+#define   CONTENTS_UNUSED               0x200
+#define   CONTENTS_BLOCKLIGHT           0x400
+#define   CONTENTS_TEAM1                0x800
+#define   CONTENTS_TEAM2                0x1000
+#define   CONTENTS_IGNORE_NODRAW_OPAQUE 0x2000
+#define   CONTENTS_MOVEABLE             0x4000
+#define   CONTENTS_AREAPORTAL           0x8000
+#define   CONTENTS_PLAYERCLIP           0x10000
+#define   CONTENTS_MONSTERCLIP          0x20000
+#define   CONTENTS_CURRENT_0            0x40000
+#define   CONTENTS_CURRENT_90           0x80000
+#define   CONTENTS_CURRENT_180          0x100000
+#define   CONTENTS_CURRENT_270          0x200000
+#define   CONTENTS_CURRENT_UP           0x400000
+#define   CONTENTS_CURRENT_DOWN         0x800000
+
+#define   CONTENTS_ORIGIN               0x1000000
+
+#define   CONTENTS_MONSTER              0x2000000
+#define   CONTENTS_DEBRIS               0x4000000
+#define   CONTENTS_DETAIL               0x8000000
+#define   CONTENTS_TRANSLUCENT          0x10000000
+#define   CONTENTS_LADDER               0x20000000
+#define   CONTENTS_HITBOX               0x40000000
+
+#define   SURF_LIGHT                    0x0001
+#define   SURF_SKY2D                    0x0002
+#define   SURF_SKY                      0x0004
+#define   SURF_WARP                     0x0008
+#define   SURF_TRANS                    0x0010
+#define   SURF_NOPORTAL                 0x0020
+#define   SURF_TRIGGER                  0x0040
+#define   SURF_NODRAW                   0x0080
+
+#define   SURF_HINT                     0x0100
+
+#define   SURF_SKIP                     0x0200
+#define   SURF_NOLIGHT                  0x0400
+#define   SURF_BUMPLIGHT                0x0800
+#define   SURF_NOSHADOWS                0x1000
+#define   SURF_NODECALS                 0x2000
+#define   SURF_NOPAINT                  SURF_NODECALS
+#define   SURF_NOCHOP                   0x4000
+#define   SURF_HITBOX                   0x8000
+
+// -----------------------------------------------------
+// spatial content masks - used for spatial queries (traceline,etc.)
+// -----------------------------------------------------
+#define   MASK_ALL                      (0xFFFFFFFF)
+#define   MASK_SOLID                    (CONTENTS_SOLID|CONTENTS_MOVEABLE|CONTENTS_WINDOW|CONTENTS_MONSTER|CONTENTS_GRATE)
+#define   MASK_PLAYERSOLID              (CONTENTS_SOLID|CONTENTS_MOVEABLE|CONTENTS_PLAYERCLIP|CONTENTS_WINDOW|CONTENTS_MONSTER|CONTENTS_GRATE)
+#define   MASK_NPCSOLID                 (CONTENTS_SOLID|CONTENTS_MOVEABLE|CONTENTS_MONSTERCLIP|CONTENTS_WINDOW|CONTENTS_MONSTER|CONTENTS_GRATE)
+#define   MASK_NPCFLUID                 (CONTENTS_SOLID|CONTENTS_MOVEABLE|CONTENTS_MONSTERCLIP|CONTENTS_WINDOW|CONTENTS_MONSTER)
+#define   MASK_WATER                    (CONTENTS_WATER|CONTENTS_MOVEABLE|CONTENTS_SLIME)
+#define   MASK_OPAQUE                   (CONTENTS_SOLID|CONTENTS_MOVEABLE|CONTENTS_OPAQUE)
+#define   MASK_OPAQUE_AND_NPCS          (MASK_OPAQUE|CONTENTS_MONSTER)
+#define   MASK_BLOCKLOS                 (CONTENTS_SOLID|CONTENTS_MOVEABLE|CONTENTS_BLOCKLOS)
+#define   MASK_BLOCKLOS_AND_NPCS        (MASK_BLOCKLOS|CONTENTS_MONSTER)
+#define   MASK_VISIBLE                  (MASK_OPAQUE|CONTENTS_IGNORE_NODRAW_OPAQUE)
+#define   MASK_VISIBLE_AND_NPCS         (MASK_OPAQUE_AND_NPCS|CONTENTS_IGNORE_NODRAW_OPAQUE)
+#define   MASK_SHOT                     (CONTENTS_SOLID|CONTENTS_MOVEABLE|CONTENTS_MONSTER|CONTENTS_WINDOW|CONTENTS_DEBRIS|CONTENTS_HITBOX)
+#define   MASK_SHOT_BRUSHONLY           (CONTENTS_SOLID|CONTENTS_MOVEABLE|CONTENTS_WINDOW|CONTENTS_DEBRIS)
+#define   MASK_SHOT_HULL                (CONTENTS_SOLID|CONTENTS_MOVEABLE|CONTENTS_MONSTER|CONTENTS_WINDOW|CONTENTS_DEBRIS|CONTENTS_GRATE)
+#define   MASK_SHOT_PORTAL              (CONTENTS_SOLID|CONTENTS_MOVEABLE|CONTENTS_WINDOW|CONTENTS_MONSTER)
+#define   MASK_SOLID_BRUSHONLY          (CONTENTS_SOLID|CONTENTS_MOVEABLE|CONTENTS_WINDOW|CONTENTS_GRATE)
+#define   MASK_PLAYERSOLID_BRUSHONLY    (CONTENTS_SOLID|CONTENTS_MOVEABLE|CONTENTS_WINDOW|CONTENTS_PLAYERCLIP|CONTENTS_GRATE)
+#define   MASK_NPCSOLID_BRUSHONLY       (CONTENTS_SOLID|CONTENTS_MOVEABLE|CONTENTS_WINDOW|CONTENTS_MONSTERCLIP|CONTENTS_GRATE)
+#define   MASK_NPCWORLDSTATIC           (CONTENTS_SOLID|CONTENTS_WINDOW|CONTENTS_MONSTERCLIP|CONTENTS_GRATE)
+#define   MASK_NPCWORLDSTATIC_FLUID     (CONTENTS_SOLID|CONTENTS_WINDOW|CONTENTS_MONSTERCLIP)
+#define   MASK_SPLITAREAPORTAL          (CONTENTS_WATER|CONTENTS_SLIME)
+#define   MASK_CURRENT                  (CONTENTS_CURRENT_0|CONTENTS_CURRENT_90|CONTENTS_CURRENT_180|CONTENTS_CURRENT_270|CONTENTS_CURRENT_UP|CONTENTS_CURRENT_DOWN)
+#define   MASK_DEADSOLID                (CONTENTS_SOLID|CONTENTS_PLAYERCLIP|CONTENTS_WINDOW|CONTENTS_GRATE)
+
+struct Plane
+{
+	Vector normal;
+	float dist;
+	unsigned char type;
+	unsigned char signbits;
+	unsigned char pad[2];
+};
+
+struct Surface
+{
+	const char *name;
+	short surfaceProps;
+	unsigned short flags;
+};
+
+struct Trace
+{
+	Vector startpos;
+	Vector endpos;
+	Plane plane;
+
+	float fraction;
+
+	int contents;
+	unsigned int dispFlags;
+
+	bool allsolid;
+	bool startsolid;
+
+	float fractionleftsolid;
+
+	Surface surface;
+
+	HitGroups hitgroup;
+	short physicsbone;
+
+	unsigned short worldSurfaceIndex;
+	Player* m_pEntityHit;
+	int hitbox;
+};
+
+struct Ray
+{
+	VectorAligned m_Start;
+	VectorAligned m_Delta;
+	VectorAligned m_StartOffset;
+	VectorAligned m_Extents;
+
+	const matrix3x4_t *m_pWorldAxisTransform;
+
+	bool m_IsRay;
+	bool m_IsSwept;
+
+	Ray() : m_pWorldAxisTransform(nullptr)
+	{}
+
+	void Init(Vector vecStart, Vector vecEnd)
+	{
+		m_Delta = vecEnd - vecStart;
+		m_IsSwept = (m_Delta.LengthSqr() != 0);
+		m_Extents.x = m_Extents.y = m_Extents.z = 0.0f;
+		m_pWorldAxisTransform = nullptr;
+		m_IsRay = true;
+		m_StartOffset.x = m_StartOffset.y = m_StartOffset.z = 0.0f;
+		m_Start = vecStart;
+	}
+
+	void Init(Vector const &start, Vector const &end, Vector const &mins, Vector const &maxs)
+	{
+		m_Delta = end - start;
+
+		m_pWorldAxisTransform = NULL;
+		m_IsSwept = (m_Delta.LengthSqr() != 0);
+
+		m_Extents = maxs - mins;
+		m_Extents *= 0.5f;
+		m_IsRay = (m_Extents.LengthSqr() < 1e-6);
+
+		// Offset m_Start to be in the center of the box...
+		m_StartOffset = maxs + mins;
+		m_StartOffset *= 0.5f;
+		m_Start = start + m_StartOffset;
+		m_StartOffset *= -1.0f;
+	}
+};
+
+enum class TraceType
+{
+    TRACE_EVERYTHING = 0,
+    TRACE_WORLD_ONLY,
+    TRACE_ENTITIES_ONLY,
+    TRACE_EVERYTHING_FILTER_PROPS,
+};
+
+class ITraceFilter
+{
+public:
+	virtual bool ShouldHitEntity(Player* pEntity, int contentsMask) = 0;
+
+	virtual TraceType GetTraceType() const = 0;
+};
+
+class TraceFilter : public ITraceFilter
+{
+public:
+	bool ShouldHitEntity(Player* pEntityHandle, int contentsMask)
+	{
+		return !(pEntityHandle == pSkip);
+	}
+
+    virtual TraceType GetTraceType() const
+    {
+        return TraceType::TRACE_EVERYTHING;
+    }
+
+	void *pSkip;
+};
+
+class TraceFilterEntitiesOnly : public ITraceFilter
+{
+public:
+    bool ShouldHitEntity(Player* pEntityHandle, int contentsMask)
+    {
+        return true;
+    }
+    virtual TraceType GetTraceType() const
+    {
+        return TraceType::TRACE_ENTITIES_ONLY;
+    }
+};
+
+class TraceFilterWorldOnly : public ITraceFilter
+{
+public:
+    bool ShouldHitEntity(Player* pServerEntity, int contentsMask)
+    {
+        return false;
+    }
+    virtual TraceType GetTraceType() const
+    {
+        return TraceType::TRACE_WORLD_ONLY;
+    }
+};
+
+class TraceFilterWorldAndPropsOnly : public ITraceFilter
+{
+public:
+    bool ShouldHitEntity(Player* pServerEntity, int contentsMask)
+    {
+        return false;
+    }
+    virtual TraceType GetTraceType() const
+    {
+        return TraceType::TRACE_EVERYTHING;
+    }
+};
+
+class IEngineTrace {
+public:
+	const char* TraceRay(Ray &ray, unsigned int fMask, ITraceFilter *pTraceFilter,  Trace  *pTrace) {
+		typedef const char* (*Fn)(void*, Ray&, unsigned int, ITraceFilter*, Trace*);
+		return getVirtualFunc<Fn>(this, 5)(this, ray, fMask, pTraceFilter, pTrace);
+	}
+};
